7df33ea6d632c6e623136d267fd9f70eaeb1c607
Sean Breckenridge <seanbrecke@gmail.com>
2021-05-27 21:45:48 -0700

    update package locks

--- mix.exs
+++ mix.exs
@@ -45,6 +45,7 @@ defmodule CheckerMal.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
+      {:plug, "~> 1.3"},
       {:httpoison, "~> 1.7.0"},
       {:floki, "~> 0.29.0"},
       {:jikan_ex, "~> 0.1.5"},
